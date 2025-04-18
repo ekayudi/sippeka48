2198a8c1999148dbab320d20fc0561bbcdf55daa
ekayudi <ekayudi23@gmail.com>
2025-04-18 18:44:46 +0700

    update project

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        $this->call(UserSeeder::class);
+        $this->call(SkillSeeder::class);
+        $this->call(RegistrationSeeder::class);
+        $this->call(QuestionTitleSeeder::class);
+        $this->call(SkillTestSeeder::class);
+        $this->call(SkillTestSessionSeeder::class);
+        $this->call(JurusanSeeder::class);
+        $this->call(JadwalTesSeeder::class);
+        $this->call(PengumumanSeeder::class);
+    }
+}
